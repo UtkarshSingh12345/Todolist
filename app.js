66e482b8920240e6d914e5ebda4a21e5432f2e0a
UtkarshSingh12345 <78500587+UtkarshSingh12345@users.noreply.github.com>
2021-06-02 20:49:30 +0530

    todo

--- app.js
+++ app.js
@@ -2,30 +2,24 @@ const express=require("express");
 const server = express();
 const bodyParser=require("body-parser");
 server.set('view engine', 'ejs');
-let date1="";
+let day1="";
 server.use(bodyParser.urlencoded({extended:true}));
 server.get("/",function(req,res){
-  let date= new Date();
-  let option={
+  var date= new Date();
+  var option={
     weekday: 'long',  day:'numeric',  year:'numeric', month:'long',  minute:'numeric' ,second:'numeric',
   };
-  let day=(date.toLocaleDateString("en-us", option));
-  res.render( "list",{day:day, day1:date1});
-
-server.post("/",function(req,res){
-   date1=req.body.num1;
-  res.redirect("/");
-});
-
-
-
-
-
+  var day=(date.toLocaleDateString("en-us", option));
+  res.render( "list",{day:day,day1:day1});
 });
 
   // day="this is a weekend day";
   // day="this is not a weekend day";
-
+server.post("/",function(req,res)
+{
+  day1=req.body.num1;
+  res.redirect("/");
+});
 
 
 
