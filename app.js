dd91c8816a3f45a22a473e0ecc23ee724a997db9
UtkarshSingh12345 <78500587+UtkarshSingh12345@users.noreply.github.com>
2021-06-02 18:32:00 +0530

    the todolist

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express=require("express");
 const server = express();
 const bodyParser=require("body-parser");
 server.set('view engine', 'ejs');
+let day1="";
 server.use(bodyParser.urlencoded({extended:true}));
 server.get("/",function(req,res){
   var date= new Date();
@@ -9,19 +10,15 @@ server.get("/",function(req,res){
     weekday: 'long',  day:'numeric',  year:'numeric', month:'long',  minute:'numeric' ,second:'numeric',
   };
   var day=(date.toLocaleDateString("en-us", option));
-  res.render( "list",{day:day});
-
-
-
-
-
-
-
+  res.render( "list",{day:day,day1:day1});
 });
 
   // day="this is a weekend day";
   // day="this is not a weekend day";
-
+server.post("/",function(req,res)
+{let day1=req.body.nu1;
+  res.redirect("/");
+});
 
 
 
