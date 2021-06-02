477e1d0765caaf70199d62969440e169d8216e81
UtkarshSingh12345 <78500587+UtkarshSingh12345@users.noreply.github.com>
2021-06-02 17:29:29 +0530

    todolist experimental files

--- app.js
+++ app.js
@@ -2,16 +2,20 @@ const express=require("express");
 const server = express();
 const bodyParser=require("body-parser");
 server.set('view engine', 'ejs');
+let date1="";
 server.use(bodyParser.urlencoded({extended:true}));
 server.get("/",function(req,res){
-  var date= new Date();
-  var option={
+  let date= new Date();
+  let option={
     weekday: 'long',  day:'numeric',  year:'numeric', month:'long',  minute:'numeric' ,second:'numeric',
   };
-  var day=(date.toLocaleDateString("en-us", option));
-  res.render( "list",{day:day});
+  let day=(date.toLocaleDateString("en-us", option));
+  res.render( "list",{day:day, day1:date1});
 
-
+server.post("/",function(req,res){
+   date1=req.body.num1;
+  res.redirect("/");
+});
 
 
 
